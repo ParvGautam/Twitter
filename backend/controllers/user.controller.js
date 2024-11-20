8894134ad56a0343b5973d39ef97fb1d688e155e
Parv <parvgautam21@gmail.com>
2024-11-21 01:08:15 +0530

    3- useFollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -43,7 +43,7 @@ export const followUnfollowUser =async (req,res)=>{
             await User.findByIdAndUpdate(id,{$pull:{followers:req.user._id}});
             await User.findByIdAndUpdate(req.user._id,{$pull:{following:id}});
 
-            //TODO
+            
             res.status(200).json({message:"User Unfollowed successfully"});
          }
          else{
@@ -59,7 +59,7 @@ export const followUnfollowUser =async (req,res)=>{
 
             await newNotification.save();
 
-            //TODO
+            
             res.status(200).json({message:"User Followed successfully"});
          }
     }
--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -5,9 +5,10 @@ import { useQuery } from "@tanstack/react-query";
 
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import LoadingSpinner from "./LoadingSpinner";
+import useFollow from "../../hooks/useFollow";
 
 const RightPanel = () => {
-	const { data: suggestedUsers, isLoading, isPending } = useQuery({
+	const { data: suggestedUsers, isLoading } = useQuery({
 		queryKey: ["suggestedUsers"],
 		queryFn: async () => {
 			try {
@@ -23,6 +24,7 @@ const RightPanel = () => {
 		},
 	});
 
+	const{follow, isPending}= useFollow(); 
 	
 
 	if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
@@ -66,7 +68,7 @@ const RightPanel = () => {
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
 										onClick={(e) => {
 											e.preventDefault();
-											
+											follow(user._id)
 										}}
 									>
 										{isPending ? <LoadingSpinner size='sm' /> : "Follow"}
